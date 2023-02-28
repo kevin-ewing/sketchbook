7ce45f26fb1c78bec0e9bf3b9e0833636db08dda
Kevin Ewing <kewing@middlebury.edu>
2023-02-28 14:23:49 -0500

    Truchet changes

--- sketches/Multi-Scale Truchet Patterns/sketch.js
+++ sketches/Multi-Scale Truchet Patterns/sketch.js
@@ -1,21 +1,82 @@
-let TILES = 4;
-const LEVELS = 49;
+// Based off of https://christophercarlson.com/portfolio/multi-scale-truchet-patterns/
+// and https://archive.bridgesmathart.org/2018/bridges2018-39.html
+
 const STROKE_FACTOR = 5;
-const DIFF_TILES = 5;
-const ALPHA = 255;
 const COL1 = [0,0,80,255];
 const COL2 = [0,100,0,255];
 const BG = [0,0,0,0];
 
+const SIZE = 400
 
-let MAX_DEPTH = 3;
+let TEMP_HUE = 0;
+
+let MAX_DEPTH = 5;
 
 let shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8, shape9, shape10, shape11, shape12, shape13, shape14;
+let shapes;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(SIZE*2, SIZE*2);
   // frameRate(1);
   noLoop();
+
+  shape1 = createGraphics(SIZE, SIZE);
+  classic(shape1);
+
+  shape2 = createGraphics(SIZE, SIZE);
+  rotateShape(shape2,PI/2)
+  classic(shape2);
+
+  //DIVISION TILES
+  shape3 = createGraphics(SIZE, SIZE);
+  division(shape3)
+
+  shape4 = createGraphics(SIZE, SIZE);
+
+  rotateShape(shape4,PI/2)  
+  division(shape4);
+
+  //FOUR DOTS TILE
+  shape5 = createGraphics(SIZE, SIZE);
+  dots(shape5);
+
+  //X TILE
+  shape6 = createGraphics(SIZE, SIZE);
+  bix(shape6);
+
+  //FROWN TILES
+  shape7 = createGraphics(SIZE, SIZE);
+  frown(shape7);
+
+  shape8 = createGraphics(SIZE, SIZE);
+  rotateShape(shape8,PI/2);
+  frown(shape8);
+
+  shape9 = createGraphics(SIZE, SIZE);
+  rotateShape(shape9,PI)  
+  frown(shape9);
+
+  shape10 = createGraphics(SIZE, SIZE);
+  rotateShape(shape10,1.5*PI)  
+  frown(shape10);
+
+  //T TILES
+  shape11 = createGraphics(SIZE, SIZE);
+  tShape(shape11);
+
+  shape12 = createGraphics(SIZE, SIZE);
+  rotateShape(shape12,PI/2)  
+  tShape(shape12);
+  
+  shape13 = createGraphics(SIZE, SIZE);
+  rotateShape(shape13,PI)  
+  tShape(shape13);
+
+  shape14 = createGraphics(SIZE, SIZE);
+  rotateShape(shape14,1.5*PI)  
+  tShape(shape14);
+
+  shapes = [shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8, shape9, shape10, shape11, shape12, shape13, shape14]
 }
 
 function draw() {
@@ -43,6 +104,12 @@ function baseShape(shape){
   shape.noFill();
   shape.stroke(COL2);
   shape.strokeWeight(shape.width/STROKE_FACTOR);
+
+  // shape.strokeWeight(1)
+  // shape.stroke("Black")
+  // shape.fill("RED");
+  // shape.rect(shape.width/2, shape.height/2,shape.width*(3/5),shape.height*(3/5));
+  // shape.noStroke();
 }
 
 
@@ -93,6 +160,7 @@ function drawTilesRec(x,y, size, maxDepth){
     drawTilesRec(x+size/2,y+size/2, size/2, maxDepth-1);
     drawTilesRec(x,y+size/2, size/2, maxDepth-1);
     drawTilesRec(x+size/2,y, size/2, maxDepth-1);
+
   }
   else{
     drawShape(x,y,size)
@@ -102,66 +170,12 @@ function drawTilesRec(x,y, size, maxDepth){
 function drawShape(x,y,size){
   //FIRST TWO TILES
   imageMode(CENTER)
-  let scale_size = size*(5/3);
+  // let SIZE = size*(5/3);
 
-  shape1 = createGraphics(scale_size, scale_size);
-  classic(shape1);
 
-  shape2 = createGraphics(scale_size, scale_size);
-  rotateShape(shape2,PI/2)
-  classic(shape2);
-
-  //DIVISION TILES
-  shape3 = createGraphics(scale_size, scale_size);
-  division(shape3)
-
-  shape4 = createGraphics(scale_size, scale_size);
-
-  rotateShape(shape4,PI/2)  
-  division(shape4);
-
-  //FOUR DOTS TILE
-  shape5 = createGraphics(scale_size, scale_size);
-  dots(shape5);
-
-  //X TILE
-  shape6 = createGraphics(scale_size, scale_size);
-  bix(shape6);
-
-  //FROWN TILES
-  shape7 = createGraphics(scale_size, scale_size);
-  frown(shape7);
-
-  shape8 = createGraphics(scale_size, scale_size);
-  rotateShape(shape8,PI/2);
-  frown(shape8);
-
-  shape9 = createGraphics(scale_size, scale_size);
-  rotateShape(shape9,PI)  
-  frown(shape9);
-
-  shape10 = createGraphics(scale_size, scale_size);
-  rotateShape(shape10,1.5*PI)  
-  frown(shape10);
-
-  //T TILES
-  shape11 = createGraphics(scale_size, scale_size);
-  tShape(shape11);
-
-  shape12 = createGraphics(scale_size, scale_size);
-  rotateShape(shape12,PI/2)  
-  tShape(shape12);
-  
-  shape13 = createGraphics(scale_size, scale_size);
-  rotateShape(shape13,PI)  
-  tShape(shape13);
-
-  shape14 = createGraphics(scale_size, scale_size);
-  rotateShape(shape14,1.5*PI)  
-  tShape(shape14);
-
-  let shapes = [shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8, shape9, shape10, shape11, shape12, shape13, shape14];
   let randIndex = Math.floor(Math.random() * shapes.length);
 
-  img = image(shapes[randIndex],x+size/2,y+size/2);
+  myImg = shapes[randIndex].get();
+  myImg.resize(size*(5/3), size*(5/3));
+  image(myImg, x+size/2,y+size/2);
 }
