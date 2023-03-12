29e261f9ac210cdc4201a29ad6662c1010a55163
Kevin Ewing <kewing@middlebury.edu>
2023-03-12 15:05:26 -0400

    Updates to the invalid_username inspired sketch, some cartoon ish shaddows added

--- sketches/invalid_username Inspired/sketch.js
+++ sketches/invalid_username Inspired/sketch.js
@@ -1,6 +1,9 @@
 const TAIL_LEN = 50;
-const NUM_BALLS = 200;
-const BALL_SIZE = 40;
+const NUM_BALLS = 30;
+const BALL_SIZE = 150;
+const RANDOM_MOVEMENT_VAL = 0.6;
+const DIMENSION_STROKE = 20;
+const DIMENSION_ALPHA = 15;
 
 let balls = [];
 
@@ -9,14 +12,14 @@ let COLOR_ARRAY;
 let MAIN_COLOR;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 
   colorMode(HSB,360);
   MAIN_COLOR = color(random(0,360),200,300);
-  COLOR_ARRAY = getAnalogousColors(MAIN_COLOR);
+  COLOR_ARRAY = getAnalogousColors(MAIN_COLOR, 10);
   BG_COLOR = color(hue(MAIN_COLOR),100,200);
 
-  for (let i = 0; i < NUM_BALLS; i++) { // create 10 balls
+  for (let i = 0; i < NUM_BALLS; i++) {
     balls.push(new Ball());
   }
 }
@@ -52,8 +55,8 @@ class Ball {
     this.vy += sin(angle) * force;
 
     // Move with momentum
-    this.vx += random(-0.2, 0.2);
-    this.vy += random(-0.2, 0.2);
+    this.vx += random(-RANDOM_MOVEMENT_VAL, RANDOM_MOVEMENT_VAL);
+    this.vy += random(-RANDOM_MOVEMENT_VAL, RANDOM_MOVEMENT_VAL);
     this.vx *= 0.99; // friction
     this.vy *= 0.99;
     this.x += this.vx;
@@ -71,47 +74,47 @@ class Ball {
   display() {
     // Draw trail
     for (let i = 0; i < this.trail.length; i++) {
-      let alpha = map(i, 0, this.trail.length-1, 0, 360);
       let size = map(i, 0, this.trail.length-1, 0, BALL_SIZE);
-      fill(this.color, alpha/2); // decrease alpha by half for older points
+      let brightness_variation = map(i, 0, this.trail.length-1, 50, 0);
+      fill(hue(this.color), saturation(this.color), brightness(this.color)-brightness_variation); // decrease alpha by half for older points
       noStroke();
       ellipse(this.trail[i].x, this.trail[i].y, size);
+      noFill();
+      stroke(0, DIMENSION_ALPHA);
+      strokeWeight(DIMENSION_STROKE)
+      strokeCap(SQUARE);
+
+      arc(this.trail[i].x, this.trail[i].y, constrain(size-DIMENSION_STROKE,0, Infinity), constrain(size-DIMENSION_STROKE,0, Infinity), PI/2, PI);
+      stroke(360, DIMENSION_ALPHA);
+      arc(this.trail[i].x, this.trail[i].y, constrain(size-DIMENSION_STROKE,0, Infinity), constrain(size-DIMENSION_STROKE,0, Infinity), (3*PI)/2, 2*PI);
+
     }
 
     // Draw ball
     fill(this.color);
     noStroke(0);
-    ellipse(this.x, this.y, BALL_SIZE);
+    ellipse(this.x, this.y, BALL_SIZE);    
   }
 }
 
 
-function getAnalogousColors(myColor) {
+function getAnalogousColors(myColor, numColors) {
   let colors = [];
   
-  // Calculate the hues of the 6 analogous colors
-  let myHue = hue(myColor);
-  let hues = [];
-  hues.push(myHue - 60);
-  hues.push(myHue - 30);
-  hues.push(myHue - 15);
-  hues.push(myHue + 15);
-  hues.push(myHue + 30);
-  hues.push(myHue + 60);
-  
-  // Normalize the hues to be within the range of 0 to 360
-  for (let i = 0; i < hues.length; i++) {
-    hues[i] = (hues[i] + 360) % 360;
-  }
-  
-  // Convert the hues, saturation, and brightness to RGB hex codes
-  for (let i = 0; i < hues.length - 1; i++) {
-    colors.push(color(hues[i], saturation(myColor), brightness(myColor)));
-  }
+  // Calculate the hue step between each analogous color
+  const hueStep = 60 / (numColors - 1);
 
-  colors.push(color(hues.slice(-1)[0] , saturation(myColor), 50));
+  // Calculate the hues of the analogous colors
+  const myHue = hue(myColor);
+  for (let i = 0; i < numColors; i++) {
+    const hueValue = (myHue - 30) + (i * hueStep);
 
-  colors.push(color("#ffffff"));
+    // Normalize the hue to be within the range of 0 to 360
+    const normalizedHue = (hueValue + 360) % 360;
+
+    // Convert the hue, saturation, and brightness to RGB hex codes
+    colors.push(color(normalizedHue, saturation(myColor), brightness(myColor)));
+  }
   
   return colors;
 }
